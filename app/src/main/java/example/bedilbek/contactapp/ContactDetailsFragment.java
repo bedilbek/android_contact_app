750c3e59a74fe68b09220dd68e1cc8291c263fa1
matchanovazizbek <mafopros@gmail.com>
2018-11-25 00:18:59 +0500

    # xml file added # ContactDetailsFragment updated with usage of xml

--- app/src/main/java/example/bedilbek/contactapp/ContactDetailsFragment.java
+++ app/src/main/java/example/bedilbek/contactapp/ContactDetailsFragment.java
@@ -2,16 +2,17 @@ package example.bedilbek.contactapp;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.TypedValue;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class ContactDetailsFragment extends Fragment {
+    ImageView userImageView;
+    TextView firstNameTextView;
+    TextView lastNameTextView;
+    TextView contactNumberTextView;
 
     public static ContactDetailsFragment newInstance(int index) {
         ContactDetailsFragment f = new ContactDetailsFragment();
@@ -34,47 +35,20 @@ public class ContactDetailsFragment extends Fragment {
         if (container == null) {
             return null;
         }
+        init();
+        // user information should be putted here
+        userImageView.setBackgroundResource(R.drawable.ic_launcher_foreground);
+        firstNameTextView.setText("");
+        lastNameTextView.setText("");
+        contactNumberTextView.setText("");
+        return inflater.inflate(R.layout.contact_detail_fragment, container, false);
 
-        LinearLayout parentView = new LinearLayout(getActivity());
-        parentView.setOrientation(LinearLayout.VERTICAL);
+    }
 
-        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
-                                                                         ViewGroup.LayoutParams.MATCH_PARENT);
-        layoutParams.setMargins(80,80, 80,80);
-        parentView.setLayoutParams(layoutParams);
-
-        int padding = (int)TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
-                10, getActivity().getResources().getDisplayMetrics());
-
-        ImageView userImageView = new ImageView(getActivity());
-        userImageView.setId(R.id.userImageViewId);
-        userImageView.setBackgroundResource(R.drawable.ic_launcher_foreground); // image of user
-        parentView.addView(userImageView);
-
-        TextView firstNameTextView = new TextView(getActivity());
-        firstNameTextView.setId(R.id.firstNameTextViewId);
-        firstNameTextView.setText(""); // we can set text using index number
-        firstNameTextView.setTextSize(20);
-        firstNameTextView.setPadding(padding, padding, padding, padding);
-        firstNameTextView.setGravity(Gravity.CENTER);
-        parentView.addView(firstNameTextView);
-
-        TextView lastNameTextView = new TextView(getActivity());
-        lastNameTextView.setId(R.id.lastNameTextViewId);
-        lastNameTextView.setText("Last Name"); // we can set text using index number
-        lastNameTextView.setTextSize(20);
-        lastNameTextView.setPadding(padding, padding, padding, padding);
-        lastNameTextView.setGravity(Gravity.CENTER);
-        parentView.addView(lastNameTextView);
-
-        TextView contactNumberTextView = new TextView(getActivity());
-        contactNumberTextView.setId(R.id.contactNumberTextViewId);
-        contactNumberTextView.setText("Contact Number"); // we can set text using index number
-        contactNumberTextView.setTextSize(20);
-        contactNumberTextView.setPadding(padding, padding, padding, padding);
-        contactNumberTextView.setGravity(Gravity.CENTER);
-        parentView.addView(contactNumberTextView);
-
-        return parentView;
+    public void init() {
+        userImageView = getView().findViewById(R.id.userImageViewId);
+        firstNameTextView = getView().findViewById(R.id.firstNameTextViewId);
+        lastNameTextView = getView().findViewById(R.id.lastNameTextViewId);
+        contactNumberTextView = getView().findViewById(R.id.contactNumberTextViewId);
     }
 }
