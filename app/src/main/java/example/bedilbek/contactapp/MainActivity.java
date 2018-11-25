dd66303e8686a4aca23344782418154384711149
matchanovazizbek <mafopros@gmail.com>
2018-11-25 22:12:35 +0500

    new contacts added

--- app/src/main/java/example/bedilbek/contactapp/MainActivity.java
+++ app/src/main/java/example/bedilbek/contactapp/MainActivity.java
@@ -25,10 +25,17 @@ public class MainActivity extends AppCompatActivity {
 
     private ArrayList<Contact> initContacts() {
         ArrayList<Contact> contacts = new ArrayList<>();
-        contacts.add(new Contact("123", "catwoman", "Cat", "Woman", 12312223));
-        contacts.add(new Contact("124", "batman", "Bat", "Man", 52315823));
-//        TODO: add new contacts as given above, try to write images that are available in drawable folder
-
+        contacts.add(new Contact("123", "catwoman", "Selina", "Kyle", 12312223));
+        contacts.add(new Contact("124", "batman", "Bruce", "Wayne", 52315823));
+        contacts.add(new Contact("125", "cyborg", "Victor", "Stone", 52367823));
+        contacts.add(new Contact("126", "flash", "Bart", "Allen", 45367113));
+        contacts.add(new Contact("127", "greenarrow", "Oliver", "Queen", 92672223));
+        contacts.add(new Contact("128", "hawkgirl", "Kendra", "Sanders", 52315823));
+        contacts.add(new Contact("129", "isis", "Andrea", "Thomas", 82377823));
+        contacts.add(new Contact("130", "spiderman", "Peter", "Parker", 44367190));
+        contacts.add(new Contact("131", "superman", "Clark", "Kent", 72318823));
+        contacts.add(new Contact("132", "zatannawoman", "Zatanna", "Unknown", 34377823));
+        contacts.add(new Contact("133", "wonderwoman", "Diana", "Princess", 46368790));
         return contacts;
     }
 }
